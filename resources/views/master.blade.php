887cfe96c62afc5bd002341d23d44e174efeb2b7
QuicklyManDEV <ruben.hermosillo23@cetis107.edu.mx>
2025-11-10 11:05:56 -0700

    cafe

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>@yield('title', 'Cafete107')</title>
+    <title>@yield('title', 'Cafe107')</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <style>
         body {
@@ -29,7 +29,7 @@
     {{-- Menú superior --}}
     <nav class="navbar navbar-expand-lg">
         <div class="container">
-            <a class="navbar-brand" href="{{ url('/') }}">☕ Cafe107</a>
+            <a class="navbar-brand" href="{{ url('/') }}"> Cafe107</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
                 <span class="navbar-toggler-icon"></span>
             </button>
