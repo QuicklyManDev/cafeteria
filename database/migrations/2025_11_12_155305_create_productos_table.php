685cd3990f4e90c048d3c92a3886d8cc87218f48
QuicklyManDev <ruben.hermosillo23@cetis107.edu.mx>
2025-11-19 17:39:48 -0700

    cambiar migracion del modelo

--- database/migrations/2025_11_12_155305_create_productos_table.php
+++ database/migrations/2025_11_12_155305_create_productos_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('productos', function (Blueprint $table) {
             $table->id();
+            $table->string('nombre');
+            $table->decimal('precio', 8, 2);
+            $table->string('imagen')->nullable();
             $table->timestamps();
         });
     }
