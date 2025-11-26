efb54fbf1545c20b1964ab9812504baf054cfd1b
QuicklyManDEV <ruben.hermosillo23@cetis107.edu.mx>
2025-11-26 09:22:47 -0700

    pendiente en el diente

--- app/Http/Controllers/PedidoController.php
+++ app/Http/Controllers/PedidoController.php
@@ -6,7 +6,7 @@ use Illuminate\Http\Request;
 
 class PedidoController extends Controller
 {
-    public function agregarProducto{
+    public function agregarProducto(){
         $ordenado = Ordenado::orderBy('nombre','asc')->get();
         return view('generarPedido', compact('ordenado'));
     }
