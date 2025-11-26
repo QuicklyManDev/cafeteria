bbbe8d067820031916daed0233a55d11887f74d3
QuicklyManDEV <ruben.hermosillo23@cetis107.edu.mx>
2025-11-26 09:37:43 -0700

    pendiente el 7.2

--- app/Http/Controllers/PedidoController.php
+++ app/Http/Controllers/PedidoController.php
@@ -6,8 +6,20 @@ use Illuminate\Http\Request;
 
 class PedidoController extends Controller
 {
-    public function agregarProducto(){
+    public function getOrdenado(){
         $ordenado = Ordenado::orderBy('nombre','asc')->get();
         return view('generarPedido', compact('ordenado'));
+    }
+
+    public function agregarProducto($id){
+        $ordenado = new Ordenado();
+        $producto = Producto::find($oid);
+        $ordenado->producto_id=$producto->id;
+        $ordenado->nombre=$producto->nombre;
+        $ordenado->precio=$producto->precio;
+        $ordenado->imagen=$producto->imagen;
+        $ordenado->cantidad=1;
+        $ordenado->save();
+        return redirect('/ordenarProductos');
     }
 }
