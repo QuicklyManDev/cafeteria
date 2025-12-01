90d566b35792fc884461c587a0a3301831838738
QuicklyManDEV <ruben.hermosillo23@cetis107.edu.mx>
2025-12-01 10:37:38 -0700

    grabar pedidio

--- routes/web.php
+++ routes/web.php
@@ -21,3 +21,6 @@ Route::get('/ordenadoMas/{id}',
 
 Route::get('/ordenadoMenos/{id}',
   [PedidoController::class,'menosCantidad']);
+
+Route::post('/grabarPedido',
+  [PedidoController::class,'grabarPedido']);
